441919e93cc6ff4eb9c92330d2b49719d8031b2c
AbdaLivi <livianto@abda.co.id>
2021-06-21 23:01:16 +0700

    Initial commit

---
+++ TestLTE/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestLTE.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string userName, string Password)
+        {
+
+            if (userName == "L" && Password == "livi")
+            {
+                Session["UserName"] = userName;
+                return View("About");
+            }
+            else
+            {
+                ViewBag.Message = "Please enter valid Email ID and Password";
+                return View("Login");
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult Insert(string userName, string Password)
+        {
+            ViewBag.Message = "User Name " + userName + " Inserted ";
+
+            return View("Contact");
+        }
+
+        public ActionResult Contact()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ TestLTE/Models/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestLTE.Models
+{
+    public class UserProfile
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+
+    }
+}
